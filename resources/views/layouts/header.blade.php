aa4c5171137478476c5e61968b39654438e6c6d6
Šimon Vacek <simonvacky@email.cz>
2022-11-17 21:17:37 +0100

    First draft for menu using included views

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-expand-lg bg-light shadow">
+    <div class="container-fluid">
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('test') }}">
+                        <h1>Učební kartičky</h1>
+                    </a>
+                </li>
+            </ul>
+
+            <a href="{{ route('login') }}">
+                <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle d-flex"
+                     style="width: 60px;"
+                     alt="Avatar"/>
+            </a>
+        </div>
+    </div>
+</nav>
