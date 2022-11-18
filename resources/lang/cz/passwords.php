80ef4ddedfd58aff80ac1430be77b0d6dd98a3cd
David Chocholaty <david.chocholaty@seznam.cz>
2022-11-18 14:45:20 +0100

    config: add czech language
    This commit adds czech language to the laravel project.

---
+++ resources/lang/cz/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Vaše heslo bylo resetováno!',
+    'sent' => 'Poslali jsme vám e-mailem odkaz pro obnovení hesla!',
+    'throttled' => 'Před dalším pokusem počkejte.',
+    'token' => 'Tento token pro resetování hesla je neplatný.',
+    'user' => "Nemůžeme najít uživatele s touto e-mailovou adresou.",
+
+];
