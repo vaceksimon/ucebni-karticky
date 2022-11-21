0ae6d35fcbce02221e80b97d7ab04a7976e439ae
Tomas Bartu <tomasbartu00@gmail.com>
2022-11-21 15:21:28 +0100

    add controller for myGroup view

--- resources/views/groups/mygroups.blade.php
+++ resources/views/groups/mygroups.blade.php
@@ -11,61 +11,116 @@
                             <div class="pb-3">
                                 <h4>Spravované skupiny</h4>
                             </div>
+                            @if($role === 'teacher')
                             <div>
                                 <ul>
                                     <li><h5>Skupiny učitelů</h5></li>
                                 </ul>
                             </div>
-                            @foreach($teacherGroups as $teacherGroup)
+                            <div>
+                            </div>
+                            @foreach($t_teacherGroups as $record)
                             <div class="card mb-3">
-                                <div class="card-header">
-                                    <div class="d-flex align-items-center gap-1">
-                                        <div class="col-md-1">
-                                            <img class="rounded-circle d-block m-auto" src="{{ $teacherGroup->photo }}" style=" width: 3rem; height: 3rem;" alt="Avatar"/>
+                                <div class="card-header d-flex align-items-center">
+                                    <div class="col-6">
+                                        <div class="d-flex align-items-center gap-1">
+                                            <div class="col-2">
+                                                <img class="rounded-circle d-block m-auto" src="{{ $record->photo }}" style=" width: 3rem; height: 3rem;" alt="Avatar"/>
+                                            </div>
+                                            <div class="col-10">
+                                                <div>{{ $record->name }}</div>
+                                            </div>
                                         </div>
-                                        <div class="col-md-11">
-                                            <div>{{ $teacherGroup->name }}</div>
+                                    </div>
+                                    <div class="col-6 d-flex justify-content-end gap-3">
+                                        <div class="" style="width: 120px">
+                                            <button type="button" class="btn btn-outline-secondary btn-sm px-3 text-nowrap" data-bs-toggle="modal" data-bs-target="#exampleModal">Upravit skupinu</button>
+                                        </div>
+                                        <div class="" style="width: 120px">
+                                            <button type="button" class="btn btn-primary btn-sm px-3 text-nowrap" data-bs-toggle="modal" data-bs-target="#exampleModal">Detail skupiny</button>
                                         </div>
                                     </div>
                                 </div>
 
                                 <div class="card-body d-flex">
-                                    <div class="col row-center mx-auto my-3" style="width: 120px">
-                                        <button type="button" class="btn btn-outline-primary btn-sm px-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Přidat člena</button>
-                                    </div>
-                                    <div class="col row-center mx-auto my-3" style="width: 120px">
-                                        <button type="button" class="btn btn-outline-primary btn-sm px-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Přidat člena</button>
+                                    <div class="row">
+                                        <div>Počet učitelů: {{ $record->pocet }}</div>
+                                        <div>Popis: {{$record->description}}</div>
                                     </div>
                                 </div>
                             </div>
                             @endforeach
-
-
+                            @endif
                             <div>
                                 <ul>
                                     <li><h5>Skupiny žáků</h5></li>
                                 </ul>
                             </div>
-                            @foreach($studentGroups as $studentGroup)
-                            <div class="card">
-                                <div class="card-header">
-                                    <div class="d-flex align-items-center gap-1">
-                                        <div class="col-md-1">
-                                            <img class="rounded-circle d-block m-auto" src="{{ $studentGroup->photo }}" style=" width: 3rem; height: 3rem;" alt="Avatar"/>
+                            @if($role === 'teacher')
+                            @foreach($t_studentGroups as $record)
+                            <div class="card mb-3">
+                                <div class="card-header d-flex align-items-center">
+                                    <div class="col-6">
+                                        <div class="d-flex align-items-center gap-1">
+                                            <div class="col-2">
+                                                <img class="rounded-circle d-block m-auto" src="{{ $record->photo }}" style=" width: 3rem; height: 3rem;" alt="Avatar"/>
+                                            </div>
+                                            <div class="col-10">
+                                                <div>{{ $record->name }}</div>
+                                            </div>
                                         </div>
-                                        <div class="col-md-11">
-                                            <div>{{ $studentGroup->name }}</div>
+                                    </div>
+                                    <div class="col-6 d-flex justify-content-end gap-3">
+                                        <div class="" style="width: 120px">
+                                            <button type="button" class="btn btn-outline-secondary btn-sm px-3 text-nowrap" data-bs-toggle="modal" data-bs-target="#exampleModal">Upravit skupinu</button>
+                                        </div>
+                                        <div class="" style="width: 120px">
+                                            <button type="button" class="btn btn-primary btn-sm px-3 text-nowrap" data-bs-toggle="modal" data-bs-target="#exampleModal">Detail skupiny</button>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="card-body">
-                                    <div class="row row-center mx-auto my-3" style="width: 120px">
-                                        <button type="button" class="btn btn-outline-primary btn-sm px-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Přidat člena</button>
+                                <div class="card-body d-flex">
+                                    <div class="row">
+                                        <div>Počet žáků: {{ $record->pocet }}</div>
+                                        <div>Popis: {{$record->description}}</div>
                                     </div>
                                 </div>
                             </div>
                             @endforeach
+                            @elseif($role === 'student')
+                                @foreach($s_studentGroups as $record)
+                                    <div class="card mb-3">
+                                        <div class="card-header d-flex align-items-center">
+                                            <div class="col-6">
+                                                <div class="d-flex align-items-center gap-1">
+                                                    <div class="col-2">
+                                                        <img class="rounded-circle d-block m-auto" src="{{ $record->photo }}" style=" width: 3rem; height: 3rem;" alt="Avatar"/>
+                                                    </div>
+                                                    <div class="col-10">
+                                                        <div>{{ $record->name }}</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-6 d-flex justify-content-end gap-3">
+                                                <div class="" style="width: 120px">
+                                                    <button type="button" class="btn btn-outline-secondary btn-sm px-3 text-nowrap" data-bs-toggle="modal" data-bs-target="#exampleModal">Upravit skupinu</button>
+                                                </div>
+                                                <div class="" style="width: 120px">
+                                                    <button type="button" class="btn btn-primary btn-sm px-3 text-nowrap" data-bs-toggle="modal" data-bs-target="#exampleModal">Detail skupiny</button>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="card-body d-flex">
+                                            <div class="row">
+                                                <div>Počet žáků: {{ $record->pocet }}</div>
+                                                <div>Popis: {{$record->description}}</div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            @endif
                         </div>
                     </div>
                 </div>
