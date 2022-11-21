1a1ceb1cc33387bda418022f7cc802e07716f546
Tomas Bartu <tomasbartu00@gmail.com>
2022-11-21 15:20:57 +0100

    add myGroup view

--- app/Http/Controllers/Groups/GroupController.php
+++ app/Http/Controllers/Groups/GroupController.php
@@ -5,27 +5,53 @@ namespace App\Http\Controllers\Groups;
 use App\Http\Controllers\Controller;
 use App\Models\Group;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class GroupController extends Controller
 {
     public function show()
     {
         return view('groups.mygroups')
-            ->with('teacherGroups', $this->getTeacherGroups())
-            ->with('studentGroups', $this->getStudentGroups());
+            ->with('role', Auth::user()->account_type)
+            ->with('t_teacherGroups', $this->t_getTeachers())
+            ->with('t_studentGroups', $this->t_getStudents())
+            ->with('s_studentGroups', $this->s_getStudents());
     }
 
-    public function getTeacherGroups()
+    public function t_getTeachers()
     {
-        return Group::where('owner', '=', Auth::user()->id)
-            ->where('type', '=', Group::TEACHERS_GROUP)
+        return DB::table('groups as gr')
+            ->select('*')
+            ->addSelect(DB::raw('(SELECT COUNT(usm.user_id)
+                        FROM users_memberships usm
+                        WHERE usm.group_id = gr.id ) AS pocet'))
+            ->where('type', '=', 'teachers')
+            ->where('owner', '=', Auth::user()->id)
             ->get();
     }
 
-    public function getStudentGroups()
+    public function t_getStudents()
     {
-        return Group::where('owner', '=', Auth::user()->id)
-            ->where('type', '=', Group::STUDENTS_GROUP)
+        return DB::table('groups as gr')
+            ->select('*')
+            ->addSelect(DB::raw('(SELECT COUNT(usm.user_id)
+                        FROM users_memberships usm
+                        WHERE usm.group_id = gr.id ) AS pocet'))
+            ->where('type', '=', 'students')
+            ->where('owner', '=', Auth::user()->id)
+            ->get();
+    }
+
+    public function s_getStudents()
+    {
+        return DB::table('groups as gr')
+            ->select('*')
+            ->addSelect(DB::raw('(SELECT COUNT(usm.user_id)
+                        FROM users_memberships usm
+                        WHERE usm.group_id = mb.group_id ) AS pocet'))
+            ->rightJoin('users_memberships as mb', 'gr.id', '=', 'mb.group_id')
+            ->where('type', '=', 'students')
+            ->where('user_id', '=', Auth::user()->id)
             ->get();
     }
 }
