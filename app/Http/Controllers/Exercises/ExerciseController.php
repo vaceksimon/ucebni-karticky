71c1ceb12d0c71e91e26ebb10473a732458f5ec8
Tomas Bartu <tomasbartu00@gmail.com>
2022-11-22 22:15:00 +0100

    add middleware authorization

--- app/Http/Controllers/Exercises/ExerciseController.php
+++ app/Http/Controllers/Exercises/ExerciseController.php
@@ -8,6 +8,16 @@ use Illuminate\Support\Facades\DB;
 
 class ExerciseController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function show()
     {
         return view('exercises.myexercises')
