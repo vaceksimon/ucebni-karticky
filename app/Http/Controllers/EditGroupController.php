9291148696713a71523dae8a710d52d08a1bbbc6
David Chocholaty <david.chocholaty@seznam.cz>
2022-11-22 18:16:51 +0100

    edit-group: add constructor
    This commit adds constructor using auth.

--- app/Http/Controllers/EditGroupController.php
+++ app/Http/Controllers/EditGroupController.php
@@ -11,6 +11,16 @@ use Illuminate\Support\Facades\Session;
 
 class EditGroupController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index(Request $request)
     {
         $group_id = Session::get('group_id');
