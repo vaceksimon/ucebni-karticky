4c2e61336313f36dff7765b002b330b50374197f
Tomas Bartu <tomasbartu00@gmail.com>
2022-11-22 22:14:24 +0100

    add controller for myexercise

---
+++ app/Http/Controllers/Exercises/ExerciseController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Exercises;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+
+class ExerciseController extends Controller
+{
+    public function show()
+    {
+        return view('exercises.myexercises')
+            ->with('role', Auth::user()->account_type)
+            ->with('t_exercises', $this->t_getExercises())
+            ->with('s_exercises', $this->s_getStudents());
+    }
+
+    public function t_getExercises()
+    {
+        return DB::table('exercises AS ex')
+            ->select('*')
+            ->addSelect(DB::raw('(SELECT COUNT(*)
+                        FROM flashcards fs
+                        WHERE fs.exercise_id = ex.id) AS pocet'))
+            ->where('author', '=', Auth::user()->id)
+            ->get();
+    }
+
+    public function s_getStudents()
+    {
+        return DB::table('users AS us')
+            ->addSelect(DB::raw('(
+                SELECT COUNT(*) FROM ucebny_karticky.exercises exin
+	            JOIN ucebny_karticky.flashcards fl on fl.exercise_id = exin.id
+	            WHERE exin.id = ex.id) AS count'))
+            ->join('users_memberships AS um', 'us.id', '=', 'um.user_id')
+            ->join('groups AS gr', 'gr.id', '=', 'um.group_id')
+            ->join('assigned_exercises AS ae', 'gr.id', '=', 'ae.group_id')
+            ->join('exercises AS ex', 'ex.id', '=', 'ae.exercise_id')
+            ->where('us.id', '=', Auth::user()->id)
+            ->get();
+    }
+
+}
