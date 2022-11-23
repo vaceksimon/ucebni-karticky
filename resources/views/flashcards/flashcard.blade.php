0f14cf62ce383bb138ee72e918448f8afa941bde
Tomas Bartu <tomasbartu00@gmail.com>
2022-11-23 23:50:07 +0100

    remove garbage file and comment

--- resources/views/flashcards/flashcard.blade.php
+++ resources/views/flashcards/flashcard.blade.php
@@ -90,18 +90,5 @@
 
         getCards();
 
-        // $('.back').hide();
-        //
-        // //.delay(3000)
-        //
-        // $('.front', '.flashcard').hover(function() {
-        //     $(this).hide();
-        //     $(this).siblings('.back').addClass( "animated flipInY fast" ).show();
-        // });
-        //
-        // $('.back', '.flashcard').mouseleave(function() {
-        //     $(this).hide();
-        //     $(this).siblings('.front').addClass( "animated flipInY fast" ).show();
-        // });
     </script>
 @endsection
