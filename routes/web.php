933724de702ee3e6798e16382cb9bc74064095a6
Tomas Bartu <tomasbartu00@gmail.com>
2022-11-23 21:00:52 +0100

    add route for ajax

--- routes/web.php
+++ routes/web.php
@@ -34,8 +34,8 @@ Route::get('create-group', function () {
 })->name('create-group');
 
 
-Route::get('flashcard', [\App\Http\Controllers\Flashcards\FlashcardController::class, 'show'])->name('flashcard');
-Route::post('flashcard', [\App\Http\Controllers\Flashcards\FlashcardController::class, 'getCards'])->name('flashcard');
+Route::get('flashcard/{id}', [\App\Http\Controllers\Flashcards\FlashcardController::class, 'show'])->name('flashcard.show');
+Route::post('flashcard', [\App\Http\Controllers\Flashcards\FlashcardController::class, 'getCards'])->name('flashcard.get-cards');
 
 Route::get('myexercises', [\App\Http\Controllers\Exercises\ExerciseController::class, 'show'])->name('myexercises');
 
