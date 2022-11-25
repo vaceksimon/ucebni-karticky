90739151eeff7746e3d64fb115ea3e42dc9f1125
David Chocholaty <david.chocholaty@seznam.cz>
2022-11-25 21:54:22 +0100

    create-exercise: add constructor
    This commit adds constructor with auth.

--- app/Http/Controllers/Exercises/CreateExerciseController.php
+++ app/Http/Controllers/Exercises/CreateExerciseController.php
@@ -9,6 +9,16 @@ use Illuminate\Http\Request;
 
 class CreateExerciseController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         return view('exercises.create-exercise');
