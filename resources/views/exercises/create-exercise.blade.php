a1efe8a101e24d6dd65ca369bd8ce34ad36eb06c
David Chocholaty <david.chocholaty@seznam.cz>
2022-11-25 21:50:20 +0100

    create-exercise: add create exercise
    This commit adds basic exercise creating.

--- resources/views/exercises/create-exercise.blade.php
+++ resources/views/exercises/create-exercise.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container my-4">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-md-6">
                 <div class="card">
                     <div class="card-header">{{ __('Vytvoření cvičení') }}
                     </div>
@@ -12,7 +12,7 @@
                         <form method="POST" action="{{ route('create-exercise.store') }}">
                             @csrf
 
-                            <div class="row row-center row-cols-2">
+                            <div class="row row-center">
                                 <div>
                                     <div class="mb-3 row row-center">
                                         <label for="" class="col-form-label text-start">
@@ -43,22 +43,6 @@
                                         </div>
                                     </div>
                                 </div>
-
-                                <div>
-                                    <!-- Second column -->
-                                    <div class="row row-center row-cols-2">
-                                        <div class="row row-center" style="width: 40%">
-                                            <img src="https://mdbcdn.b-cdn.net/img/new/avatars/2.webp" class="rounded-circle d-flex px-0"
-                                                 alt="Avatar"/>
-                                        </div>
-                                        <div class="row row-center" style="width: 60%">
-                                            <div class="container my-auto">
-                                                <label class="input-group-text my-auto" style="width: 75px" for="inputGroupFile">Upravit</label>
-                                                <input type="file" class="form-control" id="inputGroupFile" hidden>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
 
                             <div class="mb-3 row row-center">
@@ -66,9 +50,11 @@
                                     {{ __('Název *') }} :
                                 </label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <input id="name" type="text" class="form-control"
-                                           name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+                                           name="name" value="{{ old('name') }}" required autocomplete="name" autofocus
+                                           oninvalid="this.setCustomValidity('Prosím zadejte název cvičení')"
+                                           oninput="setCustomValidity('')">
                                 </div>
                             </div>
 
@@ -77,34 +63,9 @@
                                     {{ __('Popis') }} :
                                 </label>
 
-                                <div class="col-md-6">
+                                <div class="col-md-12">
                                     <textarea rows="5" cols="60" id="description" name="description" class="form-control" style="height:20vh;"
-                                              required autocomplete="description" autofocus>
-                                    </textarea>
-                                </div>
-                            </div>
-
-                            <div class="col-md-12 my-5">
-                                <div class="card">
-                                    <div class="card-header">
-                                        <div class="row row-center row-cols-2">
-                                            <div>
-                                                {{ __('Kartičky') }}
-                                            </div>
-                                            <div class="row row-center">
-                                                <button type="button" class="btn btn-outline-primary btn-sm px-3 ms-auto me-0" style="width: 120px" data-bs-toggle="modal" data-bs-target="#exampleModal">Přidat kartičky</button>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="card-body">
-                                        <div class="text-center">
-                                            Vaše skupina zatím neobsahuje žádné kartičky.
-                                        </div>
-                                        <div class="row row-center mx-auto my-3" style="width: 120px">
-                                            <button type="button" class="btn btn-outline-primary btn-sm px-3" data-bs-toggle="modal" data-bs-target="#exampleModal">Přidat kartičky</button>
-                                        </div>
-                                    </div>
+                                              required autocomplete="description" autofocus></textarea>
                                 </div>
                             </div>
 
@@ -114,28 +75,12 @@
                                   - zrušit will navigate to home-page or previous page
                                   - vytvořit skupinu will navigate to moje cviceni page
                                  -->
-                                <button class="btn btn-outline-secondary btn-lg px-4 gap-3">Zrušit</button>
+                                <a href="{{ route('home') }}">
+                                    <button class="btn btn-outline-secondary btn-lg px-4 gap-3">Zrušit</button>
+                                </a>
                                 <button type="submit" class="btn btn-primary btn-lg px-3 ms-auto me-0">Vytvořit cvičení</button>
                             </div>
                         </form>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Modal -->
-            <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true" style="--bs-modal-width: 75vw;">
-                <div class="modal-dialog">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Přidání kartiček</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body">
-
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Zavřít</button>
-                        </div>
                     </div>
                 </div>
             </div>
