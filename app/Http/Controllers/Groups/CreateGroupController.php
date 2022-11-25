a6000c99491eb391800b136989357d67c0b93768
David Chocholaty <david.chocholaty@seznam.cz>
2022-11-25 10:38:35 +0100

    create-group: remove api
    This commit removes unused api.

--- app/Http/Controllers/Groups/CreateGroupController.php
+++ app/Http/Controllers/Groups/CreateGroupController.php
@@ -18,7 +18,7 @@ class CreateGroupController extends Controller
     {
         $this->middleware('auth');
     }
-    
+
     public function index()
     {
         return view('groups.create-group');
