38ba29963f4926284f9ae996fd793e7be2bd841b
David Chocholaty <david.chocholaty@seznam.cz>
2022-11-26 15:09:42 +0100

    user-statistics: add basic view structure
    This commit adds basic view structure for the fastest and the most
    successful attempts.

--- app/Http/Controllers/Statistics/UserStatisticsController.php
+++ app/Http/Controllers/Statistics/UserStatisticsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Statistics;
 
 use App\Http\Controllers\Controller;
+use App\Models\Attempt;
+use App\Models\Exercise;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -23,7 +25,33 @@ class UserStatisticsController extends Controller
 
     public function index()
     {
-        // TODO
-        return view('statistics.user-statistics');
+        $user_id = Session::get('user_id');
+        $exercise_id = Session::get('exercise_id');
+
+        // TODO user name
+        $exercise_name = Exercise::find($exercise_id, ['name']);
+
+        return view('statistics.user-statistics')
+            ->with('user_id', $user_id)
+            ->with('exercise_id', $exercise_id)
+            ->with('exercise_name', $exercise_name->name);
+    }
+
+    public function fastestAttempt($user_id, $exercise_id)
+    {
+        // SELECT * FROM ucebni_karticky.attempts WHERE user_id = '2' AND exercise_id = '14' order by spend_time limit 1;
+        $fastest_attempt = DB::table('attempts')
+            ->where('user_id', '=', $user_id)
+            ->where('exercise_id', '=', $exercise_id)
+            ->orderBy('spend_time')
+            ->limit(1)
+            ->get();
+
+        return $fastest_attempt;
+    }
+
+    public function mostSuccessfulAttempt($user_id, $exercise_id)
+    {
+        //$mostSuccessfulAttempt = DB::table('attempts')
     }
 }
--- resources/views/statistics/user-statistics.blade.php
+++ resources/views/statistics/user-statistics.blade.php
@@ -5,11 +5,75 @@
         <div class="row justify-content-center">
             <div class="col-md-10">
                 <div class="card">
-                    <div class="card-header">{{ __('Statistiky cvičení: TODO') }}
+                    <div class="card-header">{{ __('Statistiky cvičení') }}
                     </div>
 
                     <div class="card-body">
+                        <div class="mt-1 mb-3">
+                            <b>Základní informace:</b>
 
+                            <ul>
+                                <li>
+                                    <b>Cvičení</b>: <?php echo $exercise_name ?>
+                                </li>
+                                <!-- TODO user -->
+                                @if($user_id != Auth::user()->id)
+                                    <b>Uživatel:</b>
+                                @endif
+                            </ul>
+                        </div>
+
+                        <div class="my-5">
+                            <b>Nejrúspěšnější pokus</b>
+                            <ul>
+                                <li>
+                                    Datum:
+                                </li>
+                                <li>
+                                    Čas:
+                                </li>
+                                <li>
+                                    Úspěšnost:
+                                    <ul>
+                                        <li>
+                                            Počet otázek:
+                                        </li>
+                                        <li style="color: green">
+                                            Správně:
+                                        </li>
+                                        <li style="color: red">
+                                            Špatně:
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
+
+                        <div class="my-5">
+                            <b>Nejrychlejší pokus</b>
+                            <ul>
+                                <li>
+                                    Datum:
+                                </li>
+                                <li>
+                                    Čas:
+                                </li>
+                                <li>
+                                    Úspěšnost:
+                                    <ul>
+                                        <li>
+                                            Počet otázek:
+                                        </li>
+                                        <li style="color: green">
+                                            Správně:
+                                        </li>
+                                        <li style="color: red">
+                                            Špatně:
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
