0a0ddece7e2c1ca6ab22988cd93f89b001659479
David Chocholaty <david.chocholaty@seznam.cz>
2022-11-26 20:39:31 +0100

    home: remove creating option for student
    This commit replaces buttons create group and create exercise with
    my group and my exercises for the student account type users.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,12 +12,21 @@
                     <div class="col-lg-12 mx-auto">
                         <p class="lead mb-4 text-black-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
                         <div class="d-flex justify-content-evenly flex-wrap gap-3">
-                            <form>
-                                <button class="btn btn-primary btn-lg px-4 gap-3" formaction="{{ route('create-exercise') }}">Vytvořit sadu kartiček</button>
-                            </form>
-                            <form>
-                                <button class="btn btn-outline-secondary btn-lg px-4" formaction="{{ route('create-group') }}">Vytvořit skupinu</button>
-                            </form>
+                            @if(Auth::user()->account_type != 'student')
+                                <form>
+                                    <button class="btn btn-primary btn-lg px-4 gap-3" formaction="{{ route('create-exercise') }}">Vytvořit sadu kartiček</button>
+                                </form>
+                                <form>
+                                    <button class="btn btn-outline-secondary btn-lg px-4" formaction="{{ route('create-group') }}">Vytvořit skupinu</button>
+                                </form>
+                            @else
+                                <form>
+                                    <button class="btn btn-primary btn-lg px-4 gap-3" formaction="{{ route('myexercises') }}">Moje cvičení</button>
+                                </form>
+                                <form>
+                                    <button class="btn btn-outline-secondary btn-lg px-4" formaction="{{ route('mygroups') }}">Moje skupiny</button>
+                                </form>
+                            @endif
                         </div>
                     </div>
                 </div>
