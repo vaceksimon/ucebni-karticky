616617c18f96432b2a5e34e87cbfc09f6041570b
Šimon Vacek <simonvacky@email.cz>
2022-12-07 23:01:05 +0100

    Back button and displaying only non empty fields

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -4,6 +4,9 @@
     <div class="container my-4">
         <div class="row justify-content-center">
             <div class="col-md-10">
+                <a href="{{url()->previous()}}">
+                    <button class="btn btn-outline-secondary mb-2">Zpět</button>
+                </a>
                 <div class="card">
                     <div class="card-header d-flex align-items-center">
                         <div>
@@ -37,25 +40,32 @@
                                 @endif
                             </div>
                             <div class="row">
-                                <img src="{{asset($user['photo'])}}" class="rounded-circle d-flex px-0" style="width:150px; height: 150px"
+                                <img src="{{asset($user['photo'])}}" class="rounded-circle d-flex px-0"
+                                     style="width:150px; height: 150px"
                                      alt="Avatar"/>
                             </div>
                         </div>
                         @if($user['account_type'] == 'teacher')
-                        <div class="mb-3 row">
-                            <div>
-                                Titul před:
-                                {{$user['degree_front']}}
+                            <div class="mb-3 row">
+                                @if(isset($user['degree_front']))
+                                    <div>
+                                        Titul před:
+                                        {{$user['degree_front']}}
+                                    </div>
+                                @endif
+                                @if(isset($user['degree_after']))
+                                    <div>
+                                        Titul za:
+                                        {{$user['degree_after']}}
+                                    </div>
+                                @endif
+                                @if(isset($user['school']))
+                                    <div>
+                                        Škola:
+                                        {{$user['school']}}
+                                    </div>
+                                @endif
                             </div>
-                            <div>
-                                Titul za:
-                                {{$user['degree_after']}}
-                            </div>
-                            <div>
-                                Škola:
-                                {{$user['school']}}
-                            </div>
-                        </div>
                         @endif
                     </div>
                 </div>
