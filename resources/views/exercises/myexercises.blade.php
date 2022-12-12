edad62fdb95f30ca8c3fa787887fcf71cb71ca1a
Tomas Bartu <tomasbartu00@gmail.com>
2022-12-12 02:42:04 +0100

    remove spacing from assign exercise modal window

--- resources/views/exercises/myexercises.blade.php
+++ resources/views/exercises/myexercises.blade.php
@@ -232,7 +232,7 @@
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div class="modal-body">
-                            <div class="row mt-5">
+                            <div class="row">
 
                                 <div class="col-md-12">
                                     <div class="card">
