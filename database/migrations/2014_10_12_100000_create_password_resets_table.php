73b91cc1c7e2b203261921fbc7c086d9a7b265fc
David Chocholaty <david.chocholaty@seznam.cz>
2022-12-13 22:03:03 +0100

    add missing migrations comments
    This commit adds the missing documentation comments for the migrations.

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -1,5 +1,16 @@
 <?php
 
+/************************************************************/
+/*                                                          */
+/* File: 2014_10_12_100000_create_password_resets_table.php */
+/* Author: David Chocholaty <xbartu11@stud.fit.vutbr.cz>    */
+/* Project: Project for the course ITU                      */
+/* Description: The migration for creating the password     */
+/*              resets table (the migration is kept         */
+/*              the same as was generated).                 */
+/*                                                          */
+/************************************************************/
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
